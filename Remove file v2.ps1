1a55502129797b68cee8c7544dd431e74cb8a5c9
samblaze1 <83824883+samblaze1@users.noreply.github.com>
2021-05-07 14:46:23 +0100

    Add files via upload

---
+++ Remove file v2.ps1
@@ -0,0 +1,15 @@
+﻿
+
+$filename = "c:\users\*\appdata\local\sam.txt"
+$Filename2 = "c:\windows\temp\sam.txt"
+if (Test-Path $filename) {
+    remove-item $filename
+
+
+
+    }
+
+if (test-path $Filename2) {
+remove-item $Filename2
+
+}
